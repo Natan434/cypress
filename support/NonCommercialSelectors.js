67a27e56967b74acd16ca59ef57743450f5cc1da
TERG\ntrz00 <natan.trzeba@me.pl>
2022-01-19 14:19:28 +0100

    Add HUB/CD/Shop login details + HUB test order

--- support/NonCommercialSelectors.js
+++ support/NonCommercialSelectors.js
@@ -1,5 +1,7 @@
 export default {
 selectionList:"mat-selection-list",
 selectionListOption:"mat-list-option",
-selectionProductList:"products-body"
+selectionProductList:"products-body",
+productTitle:"mat-card-header",
+addbutton:"mat-focus-indicator mat-button mat-button-base"
 }
