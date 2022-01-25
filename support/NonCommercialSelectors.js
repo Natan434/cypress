225b2b204688136d2bb8ed830639cedd578f96c5
TERG\ntrz00 <natan.trzeba@me.pl>
2022-01-25 20:00:57 +0100

    .

--- support/NonCommercialSelectors.js
+++ support/NonCommercialSelectors.js
@@ -4,5 +4,9 @@ selectionListOption:"mat-list-option",
 selectionProductList:"products-body",
 productTitle:"mat-card-header",
 addButton:"mat-card-actions > div > button > span.mat-button-wrapper",
-cartButton:"div.top-actions-wrapper > button > span.mat-button-wrapper"
+cartButton:".top-actions-wrapper > .mat-focus-indicator > .mat-button-wrapper",
+choseEvt:".control-list > :nth-child(1) > .mat-button-wrapper",
+confirmBtn:".mat-dialog-actions > .mat-focus-indicator > .mat-button-wrapper",
+commentInput:"#mat-input-3",
+summaryBtn:"app-summary-view.ng-star-inserted > .mat-dialog-actions > .mat-accent > .mat-button-wrapper"
 }
