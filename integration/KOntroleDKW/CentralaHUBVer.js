d37a6ba494f395b257802a1b77575eda2a0a0e63
TERG\ntrz00 <natan.trzeba@me.pl>
2022-02-08 11:20:51 +0100

    ..

--- integration/KOntroleDKW/CentralaHUBVer.js
+++ integration/KOntroleDKW/CentralaHUBVer.js
@@ -1,17 +1,19 @@
 // var Selectors = require('../support/DKWSelectors.js');
-var LoginDetails = require('../support/LoginDetails.js');
+var LoginDetails = require('../../support/LoginDetails.js');
+// ("C:\Users\ntrz00\CypressAutomation\cypress\support")
 var url = 'https://itdev03.me.pl:21014/'
-
-
-
 
 
 describe( "Test" ,()=>{
     it('',()=>{
+        cy.intercept("POST","https://itdev03.me.pl:21014/Inspection/BrowseInsepctions").as("BrowseInspections")
         cy.visit(url);
         cy.login(LoginDetails.DKW.KierownikHUB.Username,LoginDetails.DKW.KierownikHUB.Password);
-        cy.get(".nav > li > a").click();
+        cy.get(".nav-label").contains("Kontrole").click();
         cy.get(".nav-second-level li a").click();
+        // cy.get("#DateFrom").type('2019-01-01');
+        // cy.get("#filterBtn").click();
+        cy.wait('@BrowseInspections').then((interception)=>{assert.isNotNull(interception.response.body, 'Inspections are loaded')})
 
 
 
