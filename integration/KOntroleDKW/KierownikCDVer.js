5062c0c8e6c3f937a0fe58832eabbaec2d7e2142
TERG\ntrz00 <natan.trzeba@me.pl>
2022-02-08 11:20:51 +0100

    ..

--- integration/KOntroleDKW/KierownikCDVer.js
+++ integration/KOntroleDKW/KierownikCDVer.js
@@ -6,14 +6,21 @@ describe( "Check if inspections are browsing" ,()=>{
     it('',()=>{
         cy.intercept("POST","https://itdev03.me.pl:21014/Inspection/BrowseInsepctions").as("BrowseInspections")
         cy.visit(url);
-        cy.VirtualUser("KierownikHUB_HU10")
+
+        //Osobna metoda do VirtualUsera
+        cy.VirtualUser("KierownikCD_CDŁ1")
         cy.login(LoginDetails.DKW.SuperAdmin.Username,LoginDetails.DKW.SuperAdmin.Password);
         cy.get(".nav-label").contains("Kontrole").click();
         cy.get(".nav-second-level li a").click();
-        cy.get("#DateFrom").clear().type('2019-01-01')
-        cy.get("#filterBtn").click();
-        cy.wait('@BrowseInspections').then((interception)=>{assert.isNotNull(interception.response.body, 'Inspections are loaded')})
-        //cy.get("#InspectionsDataTable").screenshot();
+
+        //Znaleźć sposób na wybór daty z datepickera
+         cy.get("#DateFrom").clear().type('2019-01-01')
+         cy.get("#filterBtn").click();
+         
+         cy.get("#InspectionsDataTable").find();
+         cy.wait('@BrowseInspections').then((interception)=>{assert.isNotNull(interception.response.body, 'Inspections are loaded')})
+        
+
 
 
 
